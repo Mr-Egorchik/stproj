fa0c4d45dbc3726a13c02ac47a3916571cb80bbb
Egor Chitorkin <egor.chitorckin@yandex.ru>
2022-06-29 11:58:19 +0400

    flyway

--- src/main/java/com/stproj/demo/service/GroupService.java
+++ src/main/java/com/stproj/demo/service/GroupService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
+//
 @Service
 public class GroupService {
 

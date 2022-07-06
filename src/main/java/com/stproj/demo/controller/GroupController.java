ed57011569857e1d04fada633091884eb0118bdc
Egor Chitorkin <egor.chitorckin@yandex.ru>
2022-07-06 13:31:35 +0400

    logging

--- src/main/java/com/stproj/demo/controller/GroupController.java
+++ src/main/java/com/stproj/demo/controller/GroupController.java
@@ -5,6 +5,7 @@ import com.stproj.demo.entity.Group;
 import com.stproj.demo.service.GroupService;
 import com.stproj.demo.specification.GroupSpecification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,70 +20,70 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/group")
 public class GroupController {
 
     private final GroupService groupService;
     private final ModelMapper mapper;
-    private static final Logger logger = LoggerFactory.getLogger(GroupController.class);
 
     @PostMapping("/")
     public ResponseEntity<GroupDto> saveGroup(@RequestBody GroupDto group) {
-        logger.info("Start saving group...");
+        log.info("Start saving group...");
         groupService.save(mapper.map(group, Group.class));
-        logger.info("Group is saved");
+        log.info("Group is saved");
         return new ResponseEntity<>(HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<GroupDto> deleteGroup(@PathVariable("id") UUID id) {
-        logger.info("Start deleting group...");
+        log.info("Start deleting group...");
         groupService.delete(id);
-        logger.info("Group is deleted");
+        log.info("Group is deleted");
         return new ResponseEntity<>(HttpStatus.OK);
     }
     @GetMapping("/{id}")
     public ResponseEntity<GroupDto> findGroupById(@PathVariable("id") UUID id) {
-        logger.info("Start finding group...");
+        log.info("Start finding group...");
         GroupDto group = groupService.findById(id);
-        logger.info("Group is found");
+        log.info("Group is found");
         return ResponseEntity.ok(group);
     }
     @GetMapping("")
     public ResponseEntity<List<GroupDto>> findAllGroups(@ParameterObject Pageable pageable) {
-        logger.info("Start finding groups: page - " + pageable.getPageNumber() + ", size - " + pageable.getPageSize() + ", sort - " + pageable.getSort());
+        log.info("Start finding groups: page - " + pageable.getPageNumber() + ", size - " + pageable.getPageSize() + ", sort - " + pageable.getSort());
         List<GroupDto> groups = groupService.findAll(pageable);
-        logger.info("All groups are found");
+        log.info("All groups are found");
         return ResponseEntity.ok(groups);
     }
 
     @GetMapping("/more_than_ten_students_native")
     public ResponseEntity<List<GroupDto>> getGroupsWithMoreThanTenStudentsNative() {
-        logger.info("NATIVE Start finding all groups with more than 10 students...");
+        log.info("NATIVE Start finding all groups with more than 10 students...");
         List<GroupDto> groups = groupService.getGroupsWithMoreThanTenStudentsNative();
-        logger.info("All groups with more than 10 students are found");
+        log.info("All groups with more than 10 students are found");
         return ResponseEntity.ok(groups);
     }
 
     @GetMapping("/more_than_ten_students_jpql")
     public ResponseEntity<List<GroupDto>> getGroupsWithMoreThanTenStudentsJPQL() {
-        logger.info("JPQL Start finding all groups with more than 10 students...");
+        log.info("JPQL Start finding all groups with more than 10 students...");
         List<GroupDto> groups = groupService.getGroupsWithMoreThanTenStudentsJPQL();
-        logger.info("All groups with more than 10 students are found");
+        log.info("All groups with more than 10 students are found");
         return ResponseEntity.ok(groups);
     }
 
     @GetMapping("/more_than_ten_students_crud")
     public ResponseEntity<List<GroupDto>> getGroupsWithMoreThanTenStudentsCrud() {
         List<GroupDto> groups = groupService.getGroupsWithMoreThanTenStudentsCrud();
-        logger.info("All groups with more than 10 students are found");
+        log.info("All groups with more than 10 students are found");
         return ResponseEntity.ok(groups);
     }
 
     @GetMapping("/more_than_ten_students_specification")
     public ResponseEntity<List<GroupDto>> getGroupsWithMoreThanTenStudentsSpecification() {
-        logger.info("SPECIFICATION Start finding all groups with more than 10 students...");
+        log.info("SPECIFICATION Start finding all groups with more than 10 students...");
         List<GroupDto> groups = groupService.getGroupsWithMoreThanTenStudentsSpecification(new GroupSpecification());
-        logger.info("All groups with more than 10 students are found");
+        log.info("All groups with more than 10 students are found");
         return ResponseEntity.ok(groups);
     }
 
--- src/main/java/com/stproj/demo/controller/StudentController.java
+++ src/main/java/com/stproj/demo/controller/StudentController.java
@@ -4,13 +4,12 @@ import com.stproj.demo.dto.StudentDto;
 import com.stproj.demo.entity.Student;
 import com.stproj.demo.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.api.annotations.ParameterObject;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,42 +19,42 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/student")
 public class StudentController {
 
     private final StudentService studentService;
     private final ModelMapper mapper;
-    private static final Logger logger = LoggerFactory.getLogger(GroupController.class);
 
     @PostMapping("/")
     public ResponseEntity<StudentDto> saveStudent(@RequestBody StudentDto student) {
-        logger.info("Start saving student...");
+        log.info("Start saving student...");
         studentService.save(mapper.map(student, Student.class));
-        logger.info("Student is saved");
+        log.info("Student is saved");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<StudentDto> deleteStudent(@PathVariable("id") UUID id) {
-        logger.info("Start deleting student...");
+        log.info("Start deleting student...");
         studentService.delete(id);
-        logger.info("Student is deleted");
+        log.info("Student is deleted");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<StudentDto> findStudentById(@PathVariable("id") UUID id) {
-        logger.info("Start finding student...");
+        log.info("Start finding student...");
         StudentDto student = studentService.findById(id);
-        logger.info("Student is found");
+        log.info("Student is found");
         return ResponseEntity.ok(student);
     }
 
     @GetMapping("")
     public ResponseEntity<List<StudentDto>> findAllStudents(@ParameterObject Pageable pageable) {
-        logger.info("Start finding students: page - " + pageable.getPageNumber() + ", size - " + pageable.getPageSize() + ", sort - " + pageable.getSort());
+        log.info("Start finding students: page - " + pageable.getPageNumber() + ", size - " + pageable.getPageSize() + ", sort - " + pageable.getSort());
         List<StudentDto> students = studentService.findAll(pageable);
-        logger.info("All students are found");
+        log.info("All students are found");
         return ResponseEntity.ok(students);
     }
 }

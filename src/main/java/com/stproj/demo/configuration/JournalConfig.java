deecee5a0cc0c183246c9138f6318bd258487eef
Egor Chitorkin <egor.chitorckin@yandex.ru>
2022-07-12 13:35:51 +0400

    properties

--- src/main/java/com/stproj/demo/configuration/JournalConfig.java
+++ src/main/java/com/stproj/demo/configuration/JournalConfig.java
@@ -1,28 +1,17 @@
 package com.stproj.demo.configuration;
 
-import com.stproj.demo.entity.Journal;
-import com.stproj.demo.service.JournalService;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "journal")
 @Setter
-@RequiredArgsConstructor
+@Getter
 public class JournalConfig {
 
     private String name;
     private int start;
     private String country;
-
-    private final JournalService journalService;
-
-    @Bean
-    InitializingBean saveToDb() {
-        return () -> journalService.save(new Journal(name, start, country));
-    }
 }
